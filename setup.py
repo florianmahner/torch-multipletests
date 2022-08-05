c1239acf25ad4d3134c13b01ffaaeaa3e267c208
florianmahner <florian.mahner@icloud.com>
2022-08-05 11:44:15 +0200

    update requires

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-requires = ["torch", "torchvision", "numpy"]
+requires = ["torch", "torchvision", "numpy", "statsmodels"]
 
 setup(
-    name='torch_multipletest',
+    name='torch_multipletests',
     version='0.0.1',
     author='Florian P. Mahner',
     author_email='florian.mahner@gmail.com',
     license='LICENSE',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=requires,
