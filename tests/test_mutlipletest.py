68022c0ebddaa42d851e7f0c3332a2a24dcba32a
florianmahner <florian.mahner@icloud.com>
2022-08-05 11:25:38 +0200

    add bonferroni and fdr_by correction and corresponding unit tests

--- tests/test_mutlipletest.py
+++ tests/test_mutlipletest.py
@@ -1,6 +1,7 @@
 import unittest
 import torch
-from torch_multipletest.multitest import multipletests as torch_multipletests
+import numpy as np
+from torch_multipletests.multitest import multipletests as torch_multipletests
 from statsmodels.stats.multitest import multipletests
 
 
@@ -12,31 +13,32 @@ def create_synthetic_pvals(n=100):
     scale = torch.randn(100).exp()
     cut_off = torch.Tensor([0.0])
     pvals = torch.distributions.Normal(loc, scale).cdf(cut_off)
-    return pvals
+
+    return pvals.double()
 
 
 class MultipleTestTestCase(unittest.TestCase):
-    def test_benjamin_hochberg_correction(self):
-        pvals = create_synthetic_pvals()
 
-        fdr_reject, pvals_corrected = torch_multipletests(
-            pvals, alpha=0.05, method="fdr_bh", is_sorted=False
-        )
+    def test_corrections(self):
+    
+        for method in ['bonferroni', 'fdr_bh', 'fdr_by']:
+            pvals = create_synthetic_pvals()
 
-        fdr_reject.numpy()
-        pvals_corrected = pvals_corrected.numpy()
-        pvals = pvals.numpy()
+            fdr_reject, pvals_corrected, alphacBonferroni = torch_multipletests(
+                pvals, alpha=0.05, method=method, is_sorted=False
+            )
 
-        fdr_reject_, pvals_corrected_, _, _ = multipletests(
-            pvals, alpha=0.05, method="fdr_bh", is_sorted=False, returnsorted=False
-        )
-
-        self.assertEqual(pvals.shape, pvals_corrected.shape)
-        self.assertEqual(pvals_corrected.all(), pvals_corrected.all())
-        self.assertEqual(fdr_reject.all(), fdr_reject_.all())
-
-    def test_bonferroni_correction(self):
-        self.assertEqual(1, 1)
+            fdr_reject = fdr_reject.numpy()
+            pvals_corrected = pvals_corrected.numpy()
+            pvals = pvals.numpy()
+            fdr_reject_, pvals_corrected_, alphacBonferroni_, _ = multipletests(
+                pvals, alpha=0.05, method=method, is_sorted=False
+            )
+            
+            self.assertTrue(alphacBonferroni, float(alphacBonferroni_))
+            self.assertEqual(pvals.shape, pvals_corrected.shape)
+            self.assertEqual(pvals_corrected.all(), pvals_corrected_.all())
+            self.assertEqual(fdr_reject.all(), fdr_reject_.all())
 
 
 if __name__ == "__main__":
--- torch_multipletests/multitest.py
+++ torch_multipletests/multitest.py
@@ -5,7 +5,8 @@
 import torch
 
 # TODO Maybe put this in __init__.py
-__all__ = ['multipletests']
+__all__ = ["multipletests"]
+
 
 methods = ['bonferroni', 'fdr_bh', 'fdr_by'] # only works for these right now!
 
@@ -19,22 +20,101 @@ def _ecdf_torch(x):
 
 def _check_method(method):
     if method not in methods:
-        raise ValueError('method must be one of %s' % method)
+        raise ValueError('method not recognized, must be one of %s' % method)
 
+   
+def multipletests(pvals, alpha=0.05, method='bonferroni', is_sorted=False):
+    """
+    Test results and p-value correction for multiple tests
 
-def multipletests(pvals, alpha=0.05, method='hs', is_sorted=False,
-                  returnsorted=False):
-    # TODO return_sorted not implemented yet!
+    Ported from https://www.statsmodels.org/dev/generated/statsmodels.stats.multitest.multipletests.html
+    to Pytorch
+    
+    Parameters
+    ----------
+    pvals : array_like, 1-d
+        uncorrected p-values.   Must be 1-dimensional.
+    alpha : float
+        FWER, family-wise error rate, e.g. 0.1
+    method : str
+        Method used for testing and adjustment of pvalues. Can be either the
+        full name or initial letters. Available methods are:
+
+        - `bonferroni` : one-step correction
+        - `sidak` : one-step correction
+        - `holm-sidak` : step down method using Sidak adjustments
+        - `holm` : step-down method using Bonferroni adjustments
+        - `simes-hochberg` : step-up method  (independent)
+        - `hommel` : closed method based on Simes tests (non-negative)
+        - `fdr_bh` : Benjamini/Hochberg  (non-negative)
+        - `fdr_by` : Benjamini/Yekutieli (negative)
+        - `fdr_tsbh` : two stage fdr correction (non-negative)
+        - `fdr_tsbky` : two stage fdr correction (non-negative)
+
+    is_sorted : bool
+        If False (default), the p_values will be sorted, but the corrected
+        pvalues are in the original order. If True, then it assumed that the
+        pvalues are already sorted in ascending order.
+
+    Returns
+    -------
+    reject : ndarray, boolean
+        true for hypothesis that can be rejected for given alpha
+    pvals_corrected : ndarray
+        p-values corrected for multiple tests
+
+    alphacBonf : float
+        corrected alpha for Bonferroni method
+    """
 
     _check_method(method)
 
-    reject, pvals_corrected = fdrcorrection_torch(pvals, alpha=alpha, is_sorted=is_sorted)
-    
-    return reject, pvals_corrected
-
-
-def fdrcorrection_torch(pvals, alpha=0.05, is_sorted=False):
     assert pvals.ndim == 1, "pvals must be 1-dimensional, that is of shape (n,)"
+
+    pvals = torch.as_tensor(pvals)
+
+    if not is_sorted:
+        pvals_sortind = torch.argsort(pvals)
+        pvals = torch.take(pvals, pvals_sortind)
+
+    n_permutations = len(pvals)
+    alpha_corrected_bonferroni = alpha / float(n_permutations)
+
+    if method in ['bonferroni']:
+        reject = pvals <= alpha_corrected_bonferroni
+        pvals_corrected = pvals * float(n_permutations)
+
+    elif method in ['fdr_bh']:
+        reject, pvals_corrected = fdrcorrection_torch(pvals, alpha=alpha, method='indep', is_sorted=is_sorted)
+
+    elif method in ['fdr_by']:
+        reject, pvals_corrected = fdrcorrection_torch(pvals, alpha=alpha, method='n', is_sorted=is_sorted)   
+    
+    return reject, pvals_corrected, alpha_corrected_bonferroni
+
+
+def fdrcorrection_torch(pvals, alpha=0.05, method='indep', is_sorted=False):
+    """
+    Benjamini/Hochberg (1995) False Discovery Rate (FDR) Correction procedure for multiple tests.
+    pvals is a vector of p-values.
+    alpha is the desired family-wise alpha level.
+    method : {'i', 'indep', 'p', 'poscorr', 'n', 'negcorr'}, optional
+        Which method to use for FDR correction.
+        ``{'i', 'indep', 'p', 'poscorr'}`` all refer to ``fdr_bh``
+        (Benjamini/Hochberg for independent or positively
+        correlated tests). ``{'n', 'negcorr'}`` both refer to ``fdr_by``
+        (Benjamini/Yekutieli for general or negatively correlated tests).
+        Defaults to ``'indep'``.
+    is_sorted : bool, optional
+        If False (default), the p_values will be sorted, but the corrected
+        pvalues are in the original order. If True, then it assumed that the
+        pvalues are already sorted in ascending order.
+
+    Ported from https://www.statsmodels.org/dev/generated/statsmodels.stats.multitest.fdrcorrection.html to Pytorch
+    See there for full docs and explanations!
+    """
+
+    assert method in ['i', 'indep', 'n', 'negcorr'], "method must be one of 'i', 'indep', 'n', 'negcorr'"
 
     if not is_sorted:
         pvals_sortind = torch.argsort(pvals)
@@ -43,7 +123,13 @@ def fdrcorrection_torch(pvals, alpha=0.05, is_sorted=False):
     else:
         pvals_sorted = pvals
 
-    ecdf_factor = _ecdf_torch(pvals_sorted)
+    if method in ['i', 'indep']:
+        ecdf_factor = _ecdf_torch(pvals_sorted)
+
+    elif method in ['n', 'negcorr']:
+        cm = torch.sum(1./ torch.arange(1, len(pvals_sorted) + 1, dtype=torch.float64)) 
+        ecdf_factor = _ecdf_torch(pvals_sorted) / cm
+
     reject = pvals_sorted <= ecdf_factor * alpha
 
     if reject.any():
@@ -66,6 +152,7 @@ def fdrcorrection_torch(pvals, alpha=0.05, is_sorted=False):
         reject_ = torch.empty_like(reject)
         reject_[pvals_sortind] = reject
         return reject_, pvals_corrected_
+
     else:
         return reject, pvals_corrected
 
